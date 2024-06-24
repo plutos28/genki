edb9669d6b1c6083b5bb60d331ea483e28f3e0ee
plutos28 <gamevictor28@gmail.com>
2024-06-24 18:31:57 +0300

    show the sheet when button is pressed

--- app/javascript/controllers/add_workout_plan_controller.js
+++ app/javascript/controllers/add_workout_plan_controller.js
@@ -3,8 +3,15 @@ import { Controller } from "@hotwired/stimulus"
 // Connects to data-controller="add-workout-plan"
 export default class extends Controller {
   connect() {
+    const sheet = document.querySelector('#new-workout-plan-sheet');
+    const sheetOverlay = document.querySelector('#new-workout-plan-sheet-overlay');
+
     this.element.addEventListener('click', (e) => {
       console.log("open sheet");
+
+      // show the sheet and its overlay
+      sheet.classList.remove("hidden");
+      sheetOverlay.classList.remove("hidden");
     });
   }
 }
