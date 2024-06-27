209343fd8517f882226b8bfc6692bbf047d28490
plutos28 <gamevictor28@gmail.com>
2024-06-27 18:41:07 +0300

    successfully capturing weight and height using stat model

--- app/controllers/stats_controller.rb
+++ app/controllers/stats_controller.rb
@@ -1,4 +1,29 @@
 class StatsController < ApplicationController
   def index
+    @stats = Stat.all
+    @stat = Stat.new
+  end
+
+  def new
+
+  end
+
+  def create
+    @stat = Stat.new(stat_params)
+    @stat.user_id = Current.user.id
+
+    respond_to do |format|
+      if @stat.save
+        format.html { redirect_to stats_url, notice: "Stats have been successfully saved"}
+      else
+        format.html { render :new, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  private
+
+  def stat_params
+    params.require(:stat).permit(:name, :value, :unit)
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_06_24_134219) do
+ActiveRecord::Schema[7.1].define(version: 2024_06_27_145640) do
   create_table "sessions", force: :cascade do |t|
     t.integer "user_id", null: false
     t.string "user_agent"
@@ -18,6 +18,16 @@ ActiveRecord::Schema[7.1].define(version: 2024_06_24_134219) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_sessions_on_user_id"
+  end
+
+  create_table "stats", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.string "name"
+    t.decimal "value"
+    t.string "unit"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_stats_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -30,4 +40,5 @@ ActiveRecord::Schema[7.1].define(version: 2024_06_24_134219) do
   end
 
   add_foreign_key "sessions", "users"
+  add_foreign_key "stats", "users"
 end
