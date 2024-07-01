90882e1df24fcf815a0565c3d2dbf7a535d36a7e
plutos28 <gamevictor28@gmail.com>
2024-07-01 22:20:39 +0300

    simple chart example with apexcharts

---
+++ app/javascript/controllers/charts_controller.js
@@ -0,0 +1,65 @@
+import { Controller } from "@hotwired/stimulus"
+import ApexCharts from 'apexcharts'
+
+// Connects to data-controller="charts"
+export default class extends Controller {
+  connect() {
+    console.log(this.element);
+
+    let options = {
+      series: [{
+      name: 'Net Profit',
+      data: [44, 55, 57, 56, 61, 58, 63, 60, 66]
+    }, {
+      name: 'Revenue',
+      data: [76, 85, 101, 98, 87, 105, 91, 114, 94]
+    }, {
+      name: 'Free Cash Flow',
+      data: [35, 41, 36, 26, 45, 48, 52, 53, 41]
+    }],
+      chart: {
+      type: 'bar',
+      height: 350
+    },
+    plotOptions: {
+      bar: {
+        horizontal: false,
+        columnWidth: '55%',
+        endingShape: 'rounded'
+      },
+    },
+    dataLabels: {
+      enabled: false
+    },
+    stroke: {
+      show: true,
+      width: 2,
+      colors: ['transparent']
+    },
+    xaxis: {
+      categories: ['Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct'],
+    },
+    yaxis: {
+      title: {
+        text: '$ (thousands)'
+      }
+    },
+    fill: {
+      opacity: 1
+    },
+    tooltip: {
+      y: {
+        formatter: function (val) {
+          return "$ " + val + " thousands"
+        }
+      }
+    }
+    };
+
+    let chartElm = document.querySelector("#chart");
+    console.log(chartElm);
+
+    let chart = new ApexCharts(chartElm, options);
+    chart.render();
+  }
+}
--- config/importmap.rb
+++ config/importmap.rb
@@ -5,3 +5,6 @@ pin "@hotwired/turbo-rails", to: "turbo.min.js"
 pin "@hotwired/stimulus", to: "stimulus.min.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
+pin "apexcharts" # @3.49.2
+pin "chart.js" # @4.4.3
+pin "@kurkle/color", to: "@kurkle--color.js" # @0.3.2
