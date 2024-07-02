9eaef6cd9be52b5d3fdb63bcfbded10791a15d98
plutos28 <gamevictor28@gmail.com>
2024-07-02 22:19:43 +0300

    simple job with request

--- app/jobs/generate_workout_plan_job.rb
+++ app/jobs/generate_workout_plan_job.rb
@@ -2,6 +2,16 @@ class GenerateWorkoutPlanJob < ApplicationJob
   queue_as :default
 
   def perform(data)
-    # Do something later
+    uri = URI("http://localhost:11434/api/generate")
+    http = Net::HTTP.new(uri.host, uri.port)
+    request = Net::HTTP::Post.new(uri, "Content-Type" => "application/json")
+    request.body = {
+      model: "phi3",
+      prompt: "say hi in swahili",
+      stream: false
+    }.to_json
+
+    response = http.request(request)
+    response = JSON.parse(response.body)
   end
 end
