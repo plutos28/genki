9324340d1ffc66c7f8b34f0d424f10224c701657
plutos28 <gamevictor28@gmail.com>
2024-07-25 22:23:09 +0300

    allow decimals in stats page

--- app/models/nutrition.rb
+++ app/models/nutrition.rb
@@ -1,3 +1,6 @@
 class Nutrition < ApplicationRecord
   belongs_to :user
+
+  after_create_commit -> { broadcast_append_to "nutritions", partial: "nutrition/nutrition", locals: { nutrition: self }, target: "nutritions" }
+
 end
